e2a666fc0e69edac4db9769eda5809c72bae7a1b
Kaili Nishihira <kailinishihira@gmail.com>
2017-09-15 12:12:49 -0700

    separate first promise to backend

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,98 +1,102 @@
 var apiKey = require('./../.env').apiKey;
+var DoctorModule = require('../js/doctor.js').doctorModule;
 
 $(document).ready(function() {
   $('#illness-input').submit(function() {
     event.preventDefault();
     let illness = $('#illness').val();
+    let doctorModule = new DoctorModule();
+    doctorModule.getDoctorByIllness(illness);
+
 
     $('#name').val("");
     $('#illness').val("");
     $('#results').empty();
-
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${illness}&location=47.6062%2C-122.3321%2C20&user_location=47.6062%2C-122.3321&sort=best-match-asc&skip=0&limit=10&user_key=${apiKey}`;
-
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      let bodyData = body.data;
-      console.log(bodyData);
-      if (bodyData.length === 0){
-        $('#results').text("There are no doctors that meet your criteria.");
-      } else {
-        for(let i = 0; i < bodyData.length; i++) {
-          $('#results').append(`<li>Name: ${bodyData[i].practices[0].name}</li>`);
-          $('#results').append(`<li>Address: ${bodyData[i].practices[0].visit_address.street}, ${bodyData[i].practices[0].visit_address.city}, ${bodyData[i].practices[0].visit_address.state}, ${bodyData[i].practices[0].visit_address.zip}</li>`);
-          $('#results').append(`<li>Phone: ${bodyData[i].practices[0].phones[0].number}</li>`);
-          if(`${bodyData[i].practices[0].accepts_new_patients} = true`) {
-            $('#results').append(`<li>Accepts new patients: Yes</li><br>`);
-          } else {
-            $('#results').append(`<li>Accepts new patients: No</li><br>`);
-          }
-        }
-      }
-      //unable to find any websites listed or website fields for any doctors 
-
-      }, function(error) {
-        $('#results').text(`There was an error processing your request: ${error.message}`);
-      });
+    //
+    // let promise = new Promise(function(resolve, reject) {
+    //   let request = new XMLHttpRequest();
+    //   let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${illness}&location=47.6062%2C-122.3321%2C20&user_location=47.6062%2C-122.3321&sort=best-match-asc&skip=0&limit=10&user_key=${apiKey}`;
+    //
+    //   request.onload = function() {
+    //     if (this.status === 200) {
+    //       resolve(request.response);
+    //     } else {
+    //       reject(Error(request.statusText));
+    //     }
+    //   }
+    //   request.open("GET", url, true);
+    //   request.send();
+    // });
+    //
+    // promise.then(function(response) {
+    //   let body = JSON.parse(response);
+    //   let bodyData = body.data;
+    //   console.log(bodyData);
+    //   if (bodyData.length === 0){
+    //     $('#results').text("There are no doctors that meet your criteria.");
+    //   } else {
+    //     for(let i = 0; i < bodyData.length; i++) {
+    //       $('#results').append(`<li>Name: ${bodyData[i].practices[0].name}</li>`);
+    //       $('#results').append(`<li>Address: ${bodyData[i].practices[0].visit_address.street}, ${bodyData[i].practices[0].visit_address.city}, ${bodyData[i].practices[0].visit_address.state}, ${bodyData[i].practices[0].visit_address.zip}</li>`);
+    //       $('#results').append(`<li>Phone: ${bodyData[i].practices[0].phones[0].number}</li>`);
+    //       if(`${bodyData[i].practices[0].accepts_new_patients} = true`) {
+    //         $('#results').append(`<li>Accepts new patients: Yes</li><br>`);
+    //       } else {
+    //         $('#results').append(`<li>Accepts new patients: No</li><br>`);
+    //       }
+    //     }
+    //   }
+    //   //unable to find any websites listed or website fields for any doctors
+    //
+    //   }, function(error) {
+    //     $('#results').text(`There was an error processing your request: ${error.message}`);
+    //   });
   });
 
-  $('#name-input').submit(function() {
-    event.preventDefault();
-    let name = $('#name').val();
-
-    $('#name').val("");
-    $('#illness').val("");
-    $('#results').empty();
-
-    let promise1 = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=47.6062%2C-122.3321%2C20&user_location=47.6062%2C-122.3321&sort=best-match-asc&skip=0&limit=10&user_key=${apiKey}`;
-
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-
-    promise1.then(function(response) {
-      let body = JSON.parse(response);
-      let bodyData = body.data;
-      console.log(bodyData);
-      if (bodyData.length === 0){
-        $('#results').text("There are no doctors that meet your criteria.");
-      } else {
-        for(let i = 0; i < bodyData.length; i++) {
-          $('#results').append(`<li>Name: ${bodyData[i].practices[0].name}</li>`);
-          $('#results').append(`<li>Address: ${bodyData[i].practices[0].visit_address.street}, ${bodyData[i].practices[0].visit_address.city}, ${bodyData[i].practices[0].visit_address.state}, ${bodyData[i].practices[0].visit_address.zip}</li>`);
-          $('#results').append(`<li>Phone: ${bodyData[i].practices[0].phones[0].number}</li>`);
-          if(`${bodyData[i].practices[0].accepts_new_patients} = true`) {
-            $('#results').append(`<li>Accepts new patients: Yes</li><br>`);
-          } else {
-            $('#results').append(`<li>Accepts new patients: No</li><br>`);
-          }
-        }
-      }
-
-      }, function(error) {
-        $('#results').text(`There was an error processing your request: ${error.message}`);
-      });
-  });
+  // $('#name-input').submit(function() {
+  //   event.preventDefault();
+  //   let name = $('#name').val();
+  //
+  //   $('#name').val("");
+  //   $('#illness').val("");
+  //   $('#results').empty();
+  //
+  //   let promise1 = new Promise(function(resolve, reject) {
+  //     let request = new XMLHttpRequest();
+  //     let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=47.6062%2C-122.3321%2C20&user_location=47.6062%2C-122.3321&sort=best-match-asc&skip=0&limit=10&user_key=${apiKey}`;
+  //
+  //     request.onload = function() {
+  //       if (this.status === 200) {
+  //         resolve(request.response);
+  //       } else {
+  //         reject(Error(request.statusText));
+  //       }
+  //     }
+  //     request.open("GET", url, true);
+  //     request.send();
+  //   });
+  //
+  //   promise1.then(function(response) {
+  //     let body = JSON.parse(response);
+  //     let bodyData = body.data;
+  //     console.log(bodyData);
+  //     if (bodyData.length === 0){
+  //       $('#results').text("There are no doctors that meet your criteria.");
+  //     } else {
+  //       for(let i = 0; i < bodyData.length; i++) {
+  //         $('#results').append(`<li>Name: ${bodyData[i].practices[0].name}</li>`);
+  //         $('#results').append(`<li>Address: ${bodyData[i].practices[0].visit_address.street}, ${bodyData[i].practices[0].visit_address.city}, ${bodyData[i].practices[0].visit_address.state}, ${bodyData[i].practices[0].visit_address.zip}</li>`);
+  //         $('#results').append(`<li>Phone: ${bodyData[i].practices[0].phones[0].number}</li>`);
+  //         if(`${bodyData[i].practices[0].accepts_new_patients} = true`) {
+  //           $('#results').append(`<li>Accepts new patients: Yes</li><br>`);
+  //         } else {
+  //           $('#results').append(`<li>Accepts new patients: No</li><br>`);
+  //         }
+  //       }
+  //     }
+  //
+  //     }, function(error) {
+  //       $('#results').text(`There was an error processing your request: ${error.message}`);
+  //     });
+  // });
 });
