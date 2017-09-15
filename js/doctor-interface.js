36844141369e8c5dbe5fdf0bd4d5943ceb8f84f6
Kaili Nishihira <kailinishihira@gmail.com>
2017-09-15 11:21:03 -0700

    add functionality to search for dr by name and return message if no doctors that meet search criteria.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -3,35 +3,41 @@ var apiKey = require('./../.env').apiKey;
 $(document).ready(function() {
   $('#illness-input').submit(function() {
     event.preventDefault();
-      let illness = $('#illness').val();
+    let illness = $('#illness').val();
 
-      $('#illness').val("");
-      $('#results').empty();
-      let promise = new Promise(function(resolve, reject) {
-        let request = new XMLHttpRequest();
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${illness}&location=47.6062%2C-122.3321%2C20&user_location=47.6062%2C-122.3321&sort=best-match-asc&skip=0&limit=10&user_key=${apiKey}`;
+    $('#name').val("");
+    $('#illness').val("");
+    $('#results').empty();
 
-        request.onload = function() {
-          if (this.status === 200) {
-            resolve(request.response);
-          } else {
-            reject(Error(request.statusText));
-          }
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${illness}&location=47.6062%2C-122.3321%2C20&user_location=47.6062%2C-122.3321&sort=best-match-asc&skip=0&limit=10&user_key=${apiKey}`;
+
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
         }
-        request.open("GET", url, true);
-        request.send();
-      });
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
 
-      promise.then(function(response) {
-        let body = JSON.parse(response);
-        let bodyData = body.data;
-        console.log(bodyData);
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      let bodyData = body.data;
+      console.log(bodyData);
+      if (bodyData.length === 0){
+        $('#results').text("There are no doctors that meet your criteria.");
+      } else {
         for(let i = 0; i < bodyData.length; i++) {
           $('#results').append(`<li>Name: ${bodyData[i].practices[0].name}</li>`);
           $('#results').append(`<li>Address: ${bodyData[i].practices[0].visit_address.street}, ${bodyData[i].practices[0].visit_address.city}, ${bodyData[i].practices[0].visit_address.state}, ${bodyData[i].practices[0].visit_address.zip}</li>`);
           $('#results').append(`<li>Phone: ${bodyData[i].practices[0].phones[0].number}</li>`);
           $('#results').append(`<li>Accepts new patients: ${bodyData[i].practices[0].accepts_new_patients}</li><br>`);
         }
+      }
 
       //doctor's can have multiple practies... in an array!!!
       //Doctors first and last name
@@ -40,10 +46,51 @@ $(document).ready(function() {
       //website - can't find!!!!
       //accepting new patients
 
+      }, function(error) {
+        $('#results').text(`There was an error processing your request: ${error.message}`);
+      });
+  });
+
+  $('#name-input').submit(function() {
+    event.preventDefault();
+    let name = $('#name').val();
+
+    $('#name').val("");
+    $('#illness').val("");
+    $('#results').empty();
+
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=47.6062%2C-122.3321%2C20&user_location=47.6062%2C-122.3321&sort=best-match-asc&skip=0&limit=10&user_key=${apiKey}`;
+
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      let bodyData = body.data;
+      console.log(bodyData);
+      if (bodyData.length === 0){
+        $('#results').text("There are no doctors that meet your criteria.");
+      } else {
+        for(let i = 0; i < bodyData.length; i++) {
+          $('#results').append(`<li>Name: ${bodyData[i].practices[0].name}</li>`);
+          $('#results').append(`<li>Address: ${bodyData[i].practices[0].visit_address.street}, ${bodyData[i].practices[0].visit_address.city}, ${bodyData[i].practices[0].visit_address.state}, ${bodyData[i].practices[0].visit_address.zip}</li>`);
+          $('#results').append(`<li>Phone: ${bodyData[i].practices[0].phones[0].number}</li>`);
+          $('#results').append(`<li>Accepts new patients: ${bodyData[i].practices[0].accepts_new_patients}</li><br>`);
+        }
+      }
 
       }, function(error) {
         $('#results').text(`There was an error processing your request: ${error.message}`);
       });
-    });
   });
-// });
+});
